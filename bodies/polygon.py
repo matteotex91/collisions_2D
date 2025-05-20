35a41b1f8319d5205418b7592a51a6a990d8b2ac
matteotex91 <matteotex91>
2025-05-20 18:28:45 +0200

    Update

--- bodies/polygon.py
+++ bodies/polygon.py
@@ -1,6 +1,9 @@
 from bodies.body import Body
+import numpy as np
 
 
 class polygon(Body):
+    points: np.ndarray
+
     def __init__(self):
         super().__init__()
---
+++ bodies/util/util.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def intersects(A1: np.ndarray, A2: np.ndarray, B1: np.ndarray, B2: np.ndarray) -> bool:
+    return False
