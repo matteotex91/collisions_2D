db1c445556acb436a364d3c9ea3e478c6185b4b4
matteotex91 <matteotex91>
2025-05-25 21:38:58 +0200

    Update

--- bodies/util/util.py
+++ bodies/util/util.py
@@ -3,3 +3,10 @@ import numpy as np
 
 def intersects(A1: np.ndarray, A2: np.ndarray, B1: np.ndarray, B2: np.ndarray) -> bool:
     return False
+
+
+v1 = np.array([1, 2])
+v2 = np.array([3, 4])
+
+m = np.vstack([v1, v2])
+print("stop here")
