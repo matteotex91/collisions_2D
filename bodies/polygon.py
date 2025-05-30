9bdd7bdae6d0a3ee9209ec68c7720ac01bab213a
matteotex91 <matteotex91>
2025-05-30 14:42:12 +0200

    Update

--- bodies/polygon.py
+++ bodies/polygon.py
@@ -3,7 +3,46 @@ import numpy as np
 
 
 class polygon(Body):
-    points: np.ndarray
+    points: list
+    transformed_points: list
 
-    def __init__(self):
-        super().__init__()
+    def __init__(
+        self,
+        points: list,
+        position: np.ndarray,
+        speed: np.ndarray,
+        angle: float,
+        omega: float,
+        mass: float,
+        inmom: float,
+    ):
+        super().__init__(
+            position=position,
+            speed=speed,
+            angle=angle,
+            omega=omega,
+            mass=mass,
+            inmom=inmom,
+        )
+        self.points = points
+
+    def update_transformed_points(self):
+        ct = np.cos(self.angle_)
+        st = np.sin(self.angle_)
+        R = np.array([[ct, st], [-st, ct]])
+        self.transformed_points.clear()
+        for p in self.points:
+            self.transformed_points.append(self.position_ + np.dot(R, p))
+
+
+if __name__ == "__main__":
+    points = list()
+    points.append(np.array([0, 0]))
+    points.append(np.array([1, 0]))
+    points.append(np.array([1, 1]))
+    points.append(np.array([0, 1]))
+    position = np.array([0, 0])
+    speed = np.array([0, 0])
+    p = polygon(points, position, speed, 0, 0, 1, 1)
+    p.update_transformed_points()
+    print(p.transformed_points)
--- bodies/util/util.py
+++ bodies/util/util.py
@@ -2,11 +2,18 @@ import numpy as np
 
 
 def intersects(A1: np.ndarray, A2: np.ndarray, B1: np.ndarray, B2: np.ndarray) -> bool:
-    return False
+    vA = A2 - A1
+    vB = B2 - B1
+    M = np.array([vA, vB])
+    v = A2 + B2
+    c = np.dot(np.linalg.inv(M), v)
+    return (c[0] >= 0 and c[0] <= 1) and (c[1] >= 0 and c[1] <= 1)
 
 
-v1 = np.array([1, 2])
-v2 = np.array([3, 4])
+v1 = np.array([1, 0])
+v2 = np.array([0, 0])
+v3 = np.array([0, 1])
+v4 = np.array([0, -1])
 
-m = np.vstack([v1, v2])
-print("stop here")
+
+print(intersects(v1, v2, v3, v4))
